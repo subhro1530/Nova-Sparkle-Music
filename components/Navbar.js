acf695c4b66be68795f4e5f9cafefd64eaa544c8
subhro1530 <shaswata.ssaha@gmail.com>
2023-08-30 00:04:03 +0530

    2nd commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
 import styles from "../styles/Navbar.module.css";
 
 function Navbar() {
-  return <nav className={styles.navbar}>{/* Navbar content */}</nav>;
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>Your Music App</div>
+      <ul className={styles.navLinks}>
+        <li className={styles.navLink}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={styles.navLink}>
+          <Link href="/search">Search</Link>
+        </li>
+        {/* Add more navigation items here */}
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import "../styles/globals.css"; // Import your global CSS
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
